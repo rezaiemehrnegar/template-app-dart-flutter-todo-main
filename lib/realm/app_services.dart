29321ab0e6d3b3a25e637de96685acf12a451255
rezaiemehrnegar <rezaie@gmail.com>
2024-02-19 12:58:50 +0330

    app_services.dart hasbeen edited.

--- lib/realm/app_services.dart
+++ lib/realm/app_services.dart
@@ -7,11 +7,14 @@ class AppServices with ChangeNotifier {
   App app;
   User? currentUser;
   AppServices(this.id, this.baseUrl)
-      : app = App(AppConfiguration(id, baseUrl: baseUrl));
+      : app = App(
+          AppConfiguration(id, baseUrl: baseUrl),
+        );
 
   Future<User> logInUserEmailPassword(String email, String password) async {
-    User loggedInUser =
-        await app.logIn(Credentials.emailPassword(email, password));
+    User loggedInUser = await app.logIn(
+      Credentials.emailPassword(email, password),
+    );
     currentUser = loggedInUser;
     notifyListeners();
     return loggedInUser;
@@ -20,8 +23,9 @@ class AppServices with ChangeNotifier {
   Future<User> registerUserEmailPassword(String email, String password) async {
     EmailPasswordAuthProvider authProvider = EmailPasswordAuthProvider(app);
     await authProvider.registerUser(email, password);
-    User loggedInUser =
-        await app.logIn(Credentials.emailPassword(email, password));
+    User loggedInUser = await app.logIn(
+      Credentials.emailPassword(email, password),
+    );
     currentUser = loggedInUser;
     notifyListeners();
     return loggedInUser;
